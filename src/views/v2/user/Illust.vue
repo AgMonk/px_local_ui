345c338a420859f886ec0753edd14c808a122ff1
ginstone <hjg719@139.com>
2022-11-04 16:25:20 +0800

    用户插画 - 翻页

--- src/views/v2/user/Illust.vue
+++ src/views/v2/user/Illust.vue
@@ -8,7 +8,23 @@
         <h4>点击刷新</h4>
       </div>
       <div v-else style="min-height: 500px">
+        <el-pagination v-model:current-page="page"
+                       :layout="layout"
+                       :page-size="size"
+                       :total="total"
+                       hide-on-single-page
+                       size="small"
+                       @current-change="changePage"
+        />
         <illust-card-group ref="cardGroup" @request-refresh="refresh" />
+        <el-pagination v-model:current-page="page"
+                       :layout="layout"
+                       :page-size="size"
+                       :total="total"
+                       hide-on-single-page
+                       size="small"
+                       @current-change="changePage"
+        />
       </div>
     </el-main>
     <!--    <el-footer></el-footer>-->
@@ -27,13 +43,12 @@ export default {
   data() {
     return {
       loading: true,
+      layout: "prev, pager, next, jumper",
       failed: false,
       illusts: {},
       showDialog: {},
-      params: {
-        page: 1,
-        size: 60,
-      },
+      page: 1,
+      size: 60,
       total: 10,
     }
   },
@@ -41,26 +56,31 @@ export default {
   methods: {
     ...mapActions("User", ['profileIllusts']),
     ...mapGetters("User", ['getProfile']),
+    changePage(page) {
+      console.log(page)
+      this.$router.push({query: {p: page}})
+    },
     refresh() {
       this.load(this.$route, true)
     },
     load(route, force) {
       Title.set(route.name)
       const uid = Number(route.params.uid);
-      const page = route.query.p || 1;
+      const page = Number(route.query.p || 1);
 
-      this.params.page = page;
+      this.page = page;
       this.illusts = this.getProfile()(uid).illusts
+      this.total = this.illusts.length
 
       //截取数组的起始位置
-      const start = (page - 1) * this.params.size
-      const end = Math.min(page * this.params.size, this.illusts.length)
+      const start = (page - 1) * this.size
+      const end = Math.min(page * this.size, this.illusts.length)
       const ids = this.illusts.slice(start, end)
       this.loading = true;
       this.profileIllusts({uid, ids, force}).then(res => {
         this.failed = false;
         this.$nextTick(() => {
-          this.$refs.cardGroup.clear(res.map(id => {
+          this.$refs.cardGroup && this.$refs.cardGroup.clear(res.map(id => {
             return {id}
           }))
         })
