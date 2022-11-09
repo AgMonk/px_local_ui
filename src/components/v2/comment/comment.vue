299c3e9cf62874f6e71ebe4d20b2209b82756385
ginstone <hjg719@139.com>
2022-11-09 11:55:34 +0800

    评论 - 用户和日期信息

--- src/components/v2/comment/comment.vue
+++ src/components/v2/comment/comment.vue
@@ -1,18 +1,48 @@
 <template>
   <div style="margin-bottom: 5px">
     {{ data }}
+    <!--  用户和日期信息-->
+    <div>
+      <user-avatar :uid="data.uid" />
+      <user-link :uid="data.uid" />
+      <span style="color: rgba(240,248,255,0.65)"> @ {{ data.commentDate }}</span>
+      <span v-if="data.replyToUserId">
+        回复
+        <user-link :uid="data.replyToUserId" />
+      </span>
+      :
+      <!--      todo 删除评论按钮-->
+      <!--      todo 回复楼中楼 按钮 表情贴图 或 文字回复-->
+    </div>
+    <!--正文和楼中楼    -->
+    <div style="margin-left: 40px">
+      <!--    正文-->
+      <div>
+        <span v-if="data.comment" v-html="data.comment"></span>
+        <el-image v-else :src="`https://s.pximg.net/common/images/stamp/generated-stamps/${data.stampId}_s.jpg`" style="width: 50px" />
+      </div>
+      <div v-if="isRoot && data.hasReplies">
+        <!--    todo 楼中楼-->
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import UserAvatar from "@/components/v2/user/user-avatar";
+import UserLink from "@/components/v2/user/user-link";
+
 const name = "评论"
 
 export default {
   name: "comment",
   data() {
-    return {}
+    return {
+      showRelies: false,
+    }
   },
-  components: {},
+  components: {UserLink, UserAvatar},
   computed: {},
   methods: {},
   mounted() {
